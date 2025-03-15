6d01f9b58a7324b5a3dc9b992ca02f882af15529
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-03-15 15:26:27 -0700

    updated sha

--- nnex.rb
+++ nnex.rb
@@ -2,7 +2,7 @@ class Nnex < Formula
     desc "Utility to manage and copy Swift project executables"
     homepage "https://github.com/nikolainobadi/NnExecutableKit"
     url "https://github.com/nikolainobadi/NnExecutableKit/releases/download/v0.5.0/nnex"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "6e4f27469313cb18f43384111b151f01bacea1aa61a06ae5f3fd1b6d528ed6e4"
     license "MIT"
   
     def install
