0846f2db8dfebfc896e6e74b7b539240fa503ff4
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-03-22 19:28:36 -0700

    eeuop

--- nnex.rb
+++ nnex.rb
@@ -1,8 +1,8 @@
 class Nnex < Formula
     desc "Command line tool to streamline homebrew distribution"
     homepage "https://github.com/nikolainobadi/nnex"
-    url "https://github.com/nikolainobadi/nnex/releases/download/0.1.2/nnex"
-    sha256 "7c9f607d43e8a1962cafcb5a99a65edb9b4325e2f4e886d91d535726e307eb78"
+    url "https://github.com/nikolainobadi/nnex/releases/download/1.0.0/nnex"
+    sha256 "129bb30fe45888ddb93aa3b9954a95f4f080b7d4bca5d3d3daa129a8cc94816c"
     license "MIT"
 
     def install
