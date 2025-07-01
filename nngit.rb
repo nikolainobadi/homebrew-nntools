3f10df5e8dd2051a193ef0629cb42715b4ba1399
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-06-30 19:27:45 -0700

    Update nngit formula

--- nngit.rb
+++ nngit.rb
@@ -1,8 +1,8 @@
 class Nngit < Formula
     desc "Git-wrapper command-line tool"
     homepage "https://github.com/nikolainobadi/nngit"
-    url "https://github.com/nikolainobadi/nngit/releases/download/v0.3.1/nngit"
-    sha256 "422c2b50a9e9a69f5b86c1076076c722d8f4a6962764dd433296defe195e365a"
+    url "https://github.com/nikolainobadi/nngit/releases/download/v0.3.3/nngit"
+    sha256 "f9af61e23642e4315b221e781fae5a8ea7067283c1587e67436a505d68f35470"
     license "MIT"
 
     def install
