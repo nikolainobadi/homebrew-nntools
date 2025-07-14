cab37d97602b75664ec25259429a320791a289d3
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-07-13 18:38:21 -0700

    Updated nnex formulae to v0.8.3

--- nnex.rb
+++ nnex.rb
@@ -1,8 +1,8 @@
 class Nnex < Formula
     desc "Command-line tool to streamline Swift executable packaging and distribution with Homebrew"
     homepage "https://github.com/nikolainobadi/nnex"
-    url "https://github.com/nikolainobadi/nnex/releases/download/v0.8.2/nnex"
-    sha256 "fd8bddf21af86afb9466735b2fa685c5e53e68b483078388fc94b804bab823cb"
+    url "https://github.com/nikolainobadi/nnex/releases/download/v0.8.3/nnex"
+    sha256 "950f3ba0945f684a32a057cb8baa525bcc47c907fa6f38fa97518466af7b6afb"
     license "MIT"
 
     def install
