02d02cc3168effbba1a98699293d304b202d3a99
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-08-25 17:09:50 -0700

    Test nnex release

--- nnex.rb
+++ nnex.rb
@@ -1,9 +1,19 @@
 class Nnex < Formula
     desc "Command-line tool to streamline Swift executable packaging and distribution with Homebrew"
     homepage "https://github.com/nikolainobadi/nnex"
-    url "https://github.com/nikolainobadi/nnex/releases/download/v0.8.7/nnex"
-    sha256 "b98a0f88565815acd65e95f62574177351daac9c8b71f3a2f40fba578c471121"
     license "MIT"
+
+    on_macos do
+        on_arm do
+            url "https://github.com/nikolainobadi/nnex/releases/download/v0.8.8/nnex-arm64"
+            sha256 "df5e34ef22940c8fbb8e49a1806b32b6db72523f968686b71371e3d1c5f96129"
+        end
+
+        on_intel do
+            url "https://github.com/nikolainobadi/nnex/releases/download/v0.8.8/nnex-x86_64"
+            sha256 "e37b47ea9f30efbfbe84fdae2fa073c5392937e89f7edc29ec00e685e10a765b"
+        end
+    end
 
     def install
         bin.install "nnex"
