7de95c1d161f2986d99f1d2ef55718d950494c67
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-08-25 18:40:15 -0700

    Added stronger binary stripping in nnex

--- nnex.rb
+++ nnex.rb
@@ -5,13 +5,13 @@ class Nnex < Formula
 
     on_macos do
         on_arm do
-            url "https://github.com/nikolainobadi/nnex/releases/download/v0.8.11/nnex-arm64.tar.gz"
-            sha256 "802c3755f6dbbb98972aac7d040be1a0ae2726d03cba5ea3233ef5f1464050d7"
+            url "https://github.com/nikolainobadi/nnex/releases/download/v0.8.12/nnex-arm64.tar.gz"
+            sha256 "e991d58f2067e647e559c9efae595505704c97c113c3ab376108d91629b9ec5e"
         end
 
         on_intel do
-            url "https://github.com/nikolainobadi/nnex/releases/download/v0.8.11/nnex-x86_64.tar.gz"
-            sha256 "f1fc9ff563c7485dac6bd9e7426171dc2a46e79693bd67a7ddb981ecbe367947"
+            url "https://github.com/nikolainobadi/nnex/releases/download/v0.8.12/nnex-x86_64.tar.gz"
+            sha256 "4eeef3e4be68142505007cc943e673c36aaf1d02441252e975884964e8c513f6"
         end
     end
 
