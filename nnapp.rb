206cabad517c2b43140246d1f4dc381a1454c160
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-09-02 18:53:51 -0700

    Update nnapp formula to v0.6.0

--- nnapp.rb
+++ nnapp.rb
@@ -1,9 +1,19 @@
 class Nnapp < Formula
     desc "Utility to easily manage Xcode projects and Swift Packages and quickly launch them from command-line"
     homepage "https://github.com/nikolainobadi/nnapp"
-    url "https://github.com/nikolainobadi/nnapp/releases/download/v0.5.1/nnapp"
-    sha256 "f51045282ca87c171083e1071d938814cd828408373fd10b0889390f0db9795c"
     license "MIT"
+
+    on_macos do
+        on_arm do
+            url "https://github.com/nikolainobadi/nnapp/releases/download/v0.6.0/nnapp-arm64.tar.gz"
+            sha256 "f8365f22dc1338a5157ef6dd0ef8fa8f7a97a85c92388b38291dcaa5effb5da1"
+        end
+
+        on_intel do
+            url "https://github.com/nikolainobadi/nnapp/releases/download/v0.6.0/nnapp-x86_64.tar.gz"
+            sha256 "f0ad8282d169f4197524b968e446123bdcf512a6f62a16c216a1852efafb4267"
+        end
+    end
 
     def install
         bin.install "nnapp"
