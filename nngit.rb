c6ff50b625eb380e124736759292ee6351e7bd66
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-09-27 19:11:26 -0700

    Update nngit formula to v0.7.0

--- nngit.rb
+++ nngit.rb
@@ -5,13 +5,13 @@ class Nngit < Formula
 
     on_macos do
         on_arm do
-            url "https://github.com/nikolainobadi/nngit/releases/download/v0.6.0/nngit-arm64.tar.gz"
-            sha256 "bd889bcb0a94505d52f3e3cfb24ca430d668e238e0e8e14502e3b74475a502fb"
+            url "https://github.com/nikolainobadi/nngit/releases/download/v0.7.0/nngit-arm64.tar.gz"
+            sha256 "cb8235160839b8c852a18d0307712cc3ecc1717209da6ddc13515435caba42ea"
         end
 
         on_intel do
-            url "https://github.com/nikolainobadi/nngit/releases/download/v0.6.0/nngit-x86_64.tar.gz"
-            sha256 "6938b6fc010be92ab0acdfe1a24ca15fc34ff5c2aa649850db5a2839364103f6"
+            url "https://github.com/nikolainobadi/nngit/releases/download/v0.7.0/nngit-x86_64.tar.gz"
+            sha256 "bc08c18b55c764072dfc15086ce368ce8b8ddff754a6ad65256fc5b91338fe02"
         end
     end
 
