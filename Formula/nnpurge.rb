06fa69fd2d94ded3a116ef0ccd56a2009a068345
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-11-06 11:00:14 -0800

    fix nnpurge formula location

--- Formula/nnpurge.rb
+++ Formula/nnpurge.rb
@@ -1,9 +1,19 @@
 class Nnpurge < Formula
     desc "Command-line tool for CodePurge"
     homepage "https://github.com/CodePurge/nnpurge"
-    url "https://github.com/CodePurge/nnpurge/releases/download/v0.1.0/nnpurge"
-    sha256 "81999971f331fc199ab8d5f0662a0c00d6e12307ef7d52e19107bd439cafd99a"
     license "MIT"
+
+    on_macos do
+        on_arm do
+            url "https://github.com/CodePurge/nnpurge/releases/download/v0.2.0/nnpurge-arm64.tar.gz"
+            sha256 "9f3802a966fc09c9d8aac17e1f2b98e8cd2afa9f6b01e20645b1a483a39ddc88"
+        end
+
+        on_intel do
+            url "https://github.com/CodePurge/nnpurge/releases/download/v0.2.0/nnpurge-x86_64.tar.gz"
+            sha256 "5f0e41f9bdb031035768cf6362d077a64c0be619d7768b348c65bbaa1ba259bc"
+        end
+    end
 
     def install
         bin.install "nnpurge"
