b3813416c99bca3e46dc62e462aaf9a712e597ae
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-11-08 16:48:35 -0800

    update nnex formula to v0.9.6

--- Formula/nnex.rb
+++ Formula/nnex.rb
@@ -1,25 +1,26 @@
 class Nnex < Formula
     desc "Command-line tool to streamline Swift executable packaging and distribution with Homebrew"
     homepage "https://github.com/nikolainobadi/nnex"
+    version "v0.9.6"
     license "MIT"
 
     on_macos do
         on_arm do
-            url "https://github.com/nikolainobadi/nnex/releases/download/v0.9.5/Nnex-arm64.tar.gz"
-            sha256 "04fc6ee898cd3ac2beffa6c64ef972b19c15c4686d3ae316a004e7fce2359e02"
+            url "https://github.com/nikolainobadi/nnex/releases/download/v0.9.6/nnex-arm64.tar.gz"
+            sha256 "064795c5f6ad2e8682d38bcbb595d625ad1104dd89ca47b99088457d6da2449a"
         end
 
         on_intel do
-            url "https://github.com/nikolainobadi/nnex/releases/download/v0.9.5/Nnex-x86_64.tar.gz"
-            sha256 "52398f09e7557764642bce7d8c96cd2a761e19b8a7111710e88d623e2bcffd34"
+            url "https://github.com/nikolainobadi/nnex/releases/download/v0.9.6/nnex-x86_64.tar.gz"
+            sha256 "e0412d3e9b7c24a52b0a3f4f90462344a04d14c78e3e2d59f1535445fcf22736"
         end
     end
 
     def install
-        bin.install "Nnex"
+        bin.install "nnex"
     end
 
     test do
-        system "#{bin}/Nnex", "--help"
+        system "#{bin}/nnex", "--help"
     end
 end
