3071a1cc97855e5664d7813b8dfb8041265baade
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-12-14 11:56:04 -0800

    update nnex formula to 1.2.0

--- Formula/nnex.rb
+++ Formula/nnex.rb
@@ -1,18 +1,18 @@
 class Nnex < Formula
     desc "Command-line tool to streamline Swift executable packaging and distribution with Homebrew"
     homepage "https://github.com/nikolainobadi/nnex"
-    version "1.1.0"
+    version "1.2.0"
     license "MIT"
 
     on_macos do
         on_arm do
-            url "https://github.com/nikolainobadi/nnex/releases/download/1.1.0/nnex-arm64.tar.gz"
-            sha256 "aff844f34dc892b4a6117963185e3198afc0b5ecb6f69e6a645851c8ae2d84df"
+            url "https://github.com/nikolainobadi/nnex/releases/download/1.2.0/nnex-arm64.tar.gz"
+            sha256 "9e9c482d556b91beedbbbf17c13f013d7432a16146f7930ce55d5f325e910670"
         end
 
         on_intel do
-            url "https://github.com/nikolainobadi/nnex/releases/download/1.1.0/nnex-x86_64.tar.gz"
-            sha256 "31f6e8dd9a54c869c508d285940c00b909b2ed36fe9ba81050c61e606cb5ca4f"
+            url "https://github.com/nikolainobadi/nnex/releases/download/1.2.0/nnex-x86_64.tar.gz"
+            sha256 "ecbf24c9d5e564e48665dc711dc71ad24c046b2c114ebd1978452b2902ab2a0b"
         end
     end
 
